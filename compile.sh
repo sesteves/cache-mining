d795c73caf346d2f31e0365c8bb5b965f31263bf
sesteves <sroesteves@gmail.com>
2018-01-03 19:27:40 +0000

    makes jar of .class

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 javac -d out/ -cp lib/*:out/:. src/pt/inescid/gsd/cachemining/*.java src/pt/inescid/gsd/cachemining/heuristics/*java
-jar cf lib/cache-mining.jar -C src/ .
+jar cf lib/cache-mining.jar -C out/ .
